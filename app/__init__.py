6e577d3ef648cd47d09ceeb4309701a317b38ee4
ljxxcaijing <755763949@qq.com>
2016-05-23 21:02:52 +0800

    5.23 commit

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,16 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.mail import Mail
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLalchemy
+from flask.ext.login import LoginManager
 from config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLalchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -16,10 +20,13 @@ def create_app(config_name):
     config[config_name].init_app(app)
     from  .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     return app
--- app/models.py
+++ app/models.py
@@ -1,4 +1,24 @@
 from werkzeug.security import generati_password_hash, check_password_hash
+from flask.ext.login import UserMixin
+from . import login_manager
+from flask.ext.login import login_required
+
+@app.route('/secret')
+@login_required
+def secret():
+    return 'Only authenticated users are allowed!'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    usrname = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 class User(db.Model):
     password_hash = db.Column(db.String(128))
@@ -11,5 +31,8 @@ class User(db.Model):
     def password(self, password):
         self.password_hash = generate_password_hash(password)
 
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
